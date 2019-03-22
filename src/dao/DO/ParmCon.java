25377872b31c1001e270ef8dc14832d67e8c150c
LowApe <didiaoyuan@gmail.com>
2019-03-22 14:13:09 +0800

    学生实体类和数据库字段名

--- src/dao/DO/ParmCon.java
+++ src/dao/DO/ParmCon.java
@@ -19,4 +19,25 @@ public class ParmCon {
     public static final String USERNAME = properties.getProperty("username");
     public static final String PASSWORD = properties.getProperty("password");
 
+    /**
+     * 学生表字段信息
+     */
+    public static class StudentParm{
+        public static final String XUEHAO="学号";
+        public static final String XINGMING="姓名";
+        public static final String ZHUANYEMING="专业名";
+        public static final String XINGBIE="性别";
+        public static final String CHUSHENGSHIJIAN="出生时间";
+        public static final String ZONGXUEFEN="总学分";
+        public static final String ZHAOPIAN="照片";
+        public static final String BEIZHU="备注";
+        public static final String AGE="age";
+    }
+
+    public static class GradeParm{
+        public static final String XUEHAO="学号";
+        public static final String KECHENGHAO="课程号";
+        public static final String CHENGJI="成绩";
+        public static final String XUEFEn="学分";
+    }
 }
--- src/dao/DO/StudentTable.java
+++ src/dao/DO/StudentTable.java
@@ -1,10 +1,70 @@
 package dao.DO;
 
+import java.sql.Blob;
+import java.sql.Date;
+
 public class StudentTable {
-        /*
-        * 1. String 数据 获取文件中的字段
-        *
-        *   2. 基本连接操作
-        *
-        * */
+    private String xuehao;
+    private String xingming;
+    private String zhuanyeming;
+    private int xingbie;
+    private Date chushengshijian;
+    private Blob zhaopian;
+    private int age;
+
+    public String getXuehao() {
+        return xuehao;
+    }
+
+    public void setXuehao(String xuehao) {
+        this.xuehao = xuehao;
+    }
+
+    public String getXingming() {
+        return xingming;
+    }
+
+    public void setXingming(String xingming) {
+        this.xingming = xingming;
+    }
+
+    public String getZhuanyeming() {
+        return zhuanyeming;
+    }
+
+    public void setZhuanyeming(String zhuanyeming) {
+        this.zhuanyeming = zhuanyeming;
+    }
+
+    public int getXingbie() {
+        return xingbie;
+    }
+
+    public void setXingbie(int xingbie) {
+        this.xingbie = xingbie;
+    }
+
+    public Date getChushengshijian() {
+        return chushengshijian;
+    }
+
+    public void setChushengshijian(Date chushengshijian) {
+        this.chushengshijian = chushengshijian;
+    }
+
+    public Blob getZhaopian() {
+        return zhaopian;
+    }
+
+    public void setZhaopian(Blob zhaopian) {
+        this.zhaopian = zhaopian;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
